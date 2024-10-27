710cae0263eae197066b76cb04b0ed1bce70827c
Daisuke Sakurai <d.sakurai0@gmail.com>
2024-10-27 10:20:26 +0900

    Consume the core library in todo_server.

--- todo_core/lib/src/todo_core_base.dart
+++ todo_core/lib/src/todo_core_base.dart
@@ -1,6 +1,84 @@
+import 'dart:convert' as convert;
+import 'package:sembast/sembast_io.dart';
+
 // TODO: Put public facing types in this file.
 
 /// Checks if you are awesome. Spoiler: you are.
 class Awesome {
   bool get isAwesome => true;
+}
+
+
+enum GTD_tag {
+  Now,
+  Waiting,
+  Someday,
+  Context,
+  Reference
+}
+
+class Task {
+  String text;
+  GTD_tag gtd_tag;
+
+  Map<String, Object?> to_map() {
+    return {
+      "text" : text,
+      "gtd_tag": gtd_tag.name
+    };
+  }
+
+  Task (
+    {this.text    = "",
+     this.gtd_tag = GTD_tag.Now
+    }
+  );
+}
+
+class Todo_value {
+
+  String group; // Used for creating, e.g., a "Private" group
+  String project;
+  final Task? task; // null means that this is a database store to save just a group or project name.
+
+  Map<String, Object?> to_map() {
+    return {
+      "group":   group,
+      "project": project,
+      "task":    task?.to_map(),
+    };
+  }
+
+  Todo_value(
+    {
+      this.group   = "",
+      this.project = "",
+      this.task,
+    }
+  );
+}
+
+class Todo_list {
+
+  final Database database;
+  final StoreRef<String, Map<String, Object?>> _store = stringMapStoreFactory.store('todo_list');
+
+  Future<String> add(Todo_value item) async {
+    return _store.add(database, item.to_map());
+  }
+
+  Future<String> jsonEncode() async {
+    final records = await _store.find(database);
+
+    final jsonData = {
+      for (var record in records) record.key.toString(): record.value
+    };
+
+    return convert.jsonEncode(jsonData);
+  }
+
+  Todo_list ({
+    required this.database
+  });
+
 }
--- todo_server/bin/server.dart
+++ todo_server/bin/server.dart
@@ -1,4 +1,5 @@
-import 'dart:convert' as convert;
+import 'package:todo_core/todo_core.dart';
+
 import 'dart:io';
 
 import 'package:sembast/sembast_io.dart';
@@ -62,80 +63,6 @@ Future<(InternetAddress,HttpServer)> startServer() async {
   }
 
   throw Exception("Failed to start server.");
-}
-
-enum GTD_tag {
-  Now,
-  Waiting,
-  Someday,
-  Context,
-  Reference
-}
-
-class Task {
-  String text;
-  GTD_tag gtd_tag;
-
-  Map<String, Object?> to_map() {
-    return {
-      "text" : text,
-      "gtd_tag": gtd_tag.name
-    };
-  }
-
-  Task (
-    {this.text    = "",
-     this.gtd_tag = GTD_tag.Now
-    }
-  );
-}
-
-class Todo_value {
-
-  String group; // Used for creating, e.g., a "Private" group
-  String project;
-  final Task? task; // null means that this is a database store to save just a group or project name.
-
-  Map<String, Object?> to_map() {
-    return {
-      "group":   group,
-      "project": project,
-      "task":    task?.to_map(),
-    };
-  }
-
-  Todo_value(
-    {
-      this.group   = "",
-      this.project = "",
-      this.task,
-    }
-  );
-}
-
-class Todo_list {
-
-  final Database database;
-  final StoreRef<String, Map<String, Object?>> _store = stringMapStoreFactory.store('todo_list');
-
-  Future<String> add(Todo_value item) async {
-    return _store.add(database, item.to_map());
-  }
-
-  Future<String> jsonEncode() async {
-    final records = await _store.find(database);
-
-    final jsonData = {
-      for (var record in records) record.key.toString(): record.value
-    };
-
-    return convert.jsonEncode(jsonData);
-  }
-
-  Todo_list ({
-    required this.database
-  });
-
 }
 
 void main(List<String> args) async {
